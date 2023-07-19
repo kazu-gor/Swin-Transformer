8eae7aeb053d6ae7da5852c0f0b3c0c2b1777068
kazu-gor <ik126@icloud.com>
2023-07-20 03:47:17 +0900

    fix: swin to pst

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-PYTHONPATH="./swin-transformer/:$PYTHONPATH" python3 main.py --cfg configs/pst/swin_base_patch4_window12_352_finetune_patch_selection.yaml --local_rank 0
+PYTHONPATH="./swin-transformer/:$PYTHONPATH" python3 -m torch.distributed.launch main.py --cfg ./configs/swin/swin_base_patch4_window7_224.yaml --local_rank 0
